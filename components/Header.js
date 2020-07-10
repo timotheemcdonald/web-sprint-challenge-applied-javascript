02b4b93f47d9fd037ee19a9489e0a8ea853aaa56
timotheemcdonald <timotheemcdonald@gmail.com>
2020-07-10 11:36:17 -0400

    header and tabs are done

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const header = document.createElement('header')
+    const date = document.createElement('span')
+    const hOne = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    header.appendChild(date)
+    header.appendChild(hOne)
+    header.appendChild(temp)
+
+    header.className = 'header'
+    date.className = 'date'
+    temp.className = 'temp'
+
+    hOne.textContent = 'Lambda Times'
+    date.textContent = 'MARCH 28, 2020'
+    temp.textContent = '98°'
+
+    return header
+}
+
+const headerContainer = document.querySelector('.header-container')
+const newHeader = Header()
+headerContainer.appendChild(newHeader)
+console.log('sanity check')
+console.log('newHeader')
